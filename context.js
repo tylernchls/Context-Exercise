b6d41fff2f4a3dadc44388321cba7635a0962b56
Tyler Nichols <tylernchls@gmail.com>
2016-12-10 12:20:16 -1000

    context problem finished

--- context.js
+++ context.js
@@ -16,15 +16,19 @@ var bmo = new Robot("B-Mo");
 var wall_e = new Robot("Wall-E");
 
 
-// #1
-console.log(  );
 
-// #2  
-console.log(  );
+
+// #1
+console.log(add(0,1));
+
+// #2
+console.log(add.call(voltron, 2, 3));
 
 // #3
-console.log(  );
+console.log(add.apply(optimus, [20,30]));
 
 // #4
 
-console.log(  );
+var calculate = add.bind(bender);
+console.log(calculate("drinking ", "beer"));
+
